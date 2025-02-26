5ea7cfb97871f88801084cf042e85266bf162784
Allister Driscoll <allisterdrisc@gmail.com>
2025-02-26 12:27:35 -0500

    removed .env file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import 'dotenv/config';
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
 
-app.listen(port, () => console.log('Running my app', process.env.MY_SECRET));
+app.listen(port, () => console.log('Running my app'));
