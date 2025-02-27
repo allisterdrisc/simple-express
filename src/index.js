0e7d0d050438589ae36de0ac0e3011fe150db900
Allister Driscoll <allisterdrisc@gmail.com>
2025-02-27 09:14:36 -0500

    troubleshooting port

--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ const port = 3000;
 
 app.use(express.static('public'));
 
-app.listen(port, () => console.log('Running my app'));
+//app.listen(port, () => console.log('Running my app'));
+
+app.listen(port, '0.0.0.0', () => {  // Ensuring it's bound to 0.0.0.0
+  console.log('Server running on port 3000');
+});
