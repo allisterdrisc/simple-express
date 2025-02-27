b87dd075c066e98c3cd47a8e2987711c8b3c3aea
Allister Driscoll <allisterdrisc@gmail.com>
2025-02-27 15:34:21 -0500

    added postgreSQL and some of MongoDB

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+require('dotenv').config(); // loads variables from .env
+const { Client } = require('pg');
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+
+const client = new Client({
+  connectionString: process.env.POSTGRES_URI, // loads from .env
+});
+
+client.connect()
+  .then(() => console.log('Connected to PostgreSQL'))
+  .catch(err => console.error('Connection error', err.stack));
 
 app.get('/app', (req, res) => {
   res.send('This is a dynamic route!');
