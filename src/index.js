9f8eb7901bab027f0dbd98ecd45a98d90cff8c7a
Allister Driscoll <allisterdrisc@gmail.com>
2025-03-04 13:24:02 -0500

    added more to method-override

--- src/index.js
+++ src/index.js
@@ -14,10 +14,11 @@ app.set('views', path.join(__dirname, '../views')); // adjust path to the views 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 
-// set up body parsing middleware to be able to use post etc
+// set up body parsing middleware to be able to use post, delete, put
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 
 // Validate environment variables
 const { MONGO_URI, POSTGRES_URI } = process.env;
